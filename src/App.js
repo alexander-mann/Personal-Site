b6952b675c782e38fecc070fba9628a99c780bf3
alexander-mann <amann9@myseneca.ca>
2018-06-07 18:11:17 -0400

    mobile step1

--- src/App.js
+++ src/App.js
@@ -10,62 +10,136 @@ import githubIcon from './images/github-icon.png';
 import linkedinIcon from './images/linkedin-icon.png';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      width: window.innerWidth,
+    };
+  }
+  componentWillMount() {
+    window.addEventListener('resize', this.handleWindowSizeChange);
+  }
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleWindowSizeChange);
+  }
+  handleWindowSizeChange = () => {
+    this.setState({ width: window.innerWidth });
+  };
   render() {
-    return (
-      <div className="App">
-        <script type="text/javascript" src="https://platform.linkedin.com/badges/js/profile.js" async defer></script>
-        <div className="container">
-          <div className="col-md-12">
-            {/* header */}
-            <div className="row" id="header">
-              <div className="col-md-8">
-                <NavLink to='/' id="hdr"><h1>alexander<strong>mann</strong></h1></NavLink>
-              </div>
-              <div className="col-md-4">
-                <a href='https://www.linkedin.com/in/alexandercmann/' target='_blank' rel="noopener noreferrer"><img id="linkedin" src={linkedinIcon} alt={"linkedin"} /></a>
-                <a href='https://github.com/alexander-mann' target='_blank' rel="noopener noreferrer"><img id="github" src={githubIcon} alt={"github"} /></a>
-              </div>
-            </div>
-            {/* navigation */}
-            <div className="row" id="nav">
-              <nav className="navbar mx-auto">
-                <NavLink activeClassName='is-active' to='/who'> <button className="btn btn-outline-dark" id="who" type="button">who </button> </NavLink>
-                <NavLink activeClassName='is-active' to='/what'> <button className="btn btn-outline-dark" id="what" type="button">what </button> </NavLink>
-                <NavLink activeClassName='is-active' to='/why'> <button className="btn btn-outline-dark" id="why" type="button">why</button> </NavLink>
-              </nav>
-            </div>
-            {/* content */}
-            <div className="row" id="siteBody">
-              <Switch>
-                <Route exact path='/' render={() => (
-                  <Home />
-                )} />
-                <Route exact path='/who' render={() => (
-                  <Who />
-                )} />
-                <Route exact path='/what' render={() => (
-                  <What />
-                )} />
-                <Route exact path='/why' render={() => (
-                  <Why />
-                )} />
-                <Route render={() => (
-                  <h1>Not Found</h1>
-                )} />
-              </Switch>
-            </div>
-          </div>
-        </div>
-        {/* footer */}
-        <footer>
-          <div className="container" id="footer">
+    const { width } = this.state;
+    const isMobile = width < 1000;
+    if (isMobile) {
+      return (
+        <div className="App" id="AppMobile">
+          <script type="text/javascript" src="https://platform.linkedin.com/badges/js/profile.js" async defer></script>
+          <div className="container">
             <div className="col-md-12">
-              <a id="ftr">created using <strong>React</strong></a>
+              {/* header */}
+                  <NavLink to='/' id="hdr"><h1 id="mobileHdr">alexander<strong>mann</strong></h1></NavLink>
+                  <a href='https://www.linkedin.com/in/alexandercmann/' target='_blank' rel="noopener noreferrer"><img id="mobileIcon" src={linkedinIcon} alt={"linkedin"} /></a>
+                  <a href='https://github.com/alexander-mann' target='_blank' rel="noopener noreferrer"><img id="mobileIcon" src={githubIcon} alt={"github"} /></a>
+              {/* navigation */}
+                <nav className="navbar justify-content-center">
+                  <NavLink activeClassName='is-active' to='/who'> <button className="btn btn-outline-dark" id="who" type="button">who </button> </NavLink>
+                  </nav>
+                  <nav className="navbar justify-content-center">
+                  <NavLink activeClassName='is-active' to='/what'> <button className="btn btn-outline-dark" id="what" type="button">what </button> </NavLink>
+                  </nav>
+                  <nav className="navbar justify-content-center">
+                  <NavLink activeClassName='is-active' to='/why'> <button className="btn btn-outline-dark" id="why" type="button">why</button> </NavLink>
+                </nav>
+                <hr />
+              {/* content */}
+              <div className="row" id="siteBody">
+                <Switch>
+                  <Route exact path='/' render={() => (
+                    <Home />
+                  )} />
+                  <Route exact path='/who' render={() => (
+                    <Who />
+                  )} />
+                  <Route exact path='/what' render={() => (
+                    <What />
+                  )} />
+                  <Route exact path='/why' render={() => (
+                    <Why />
+                  )} />
+                  <Route render={() => (
+                    <h1>Not Found</h1>
+                  )} />
+                </Switch>
+              </div>
             </div>
           </div>
-        </footer>
-      </div>
-    );
+          {/* footer */}
+          <hr />
+          <footer>
+            <div className="container">
+              <div className="col-md-12">
+                <a id="mobileFooter">created using <strong>React</strong></a>
+              </div>
+            </div>
+          </footer>
+        </div>
+      );
+    } else {
+      return (
+        <div className="App" id="AppDesktop">
+          <script type="text/javascript" src="https://platform.linkedin.com/badges/js/profile.js" async defer></script>
+          <div className="container">
+            <div className="col-md-12">
+              {/* header */}
+              <div className="row" id="header">
+                <div className="col-md-8">
+                  <NavLink to='/' id="hdr"><h1>alexander<strong>mann</strong></h1></NavLink>
+                </div>
+                <div className="col-md-4">
+                  <a href='https://www.linkedin.com/in/alexandercmann/' target='_blank' rel="noopener noreferrer"><img id="linkedin" src={linkedinIcon} alt={"linkedin"} /></a>
+                  <a href='https://github.com/alexander-mann' target='_blank' rel="noopener noreferrer"><img id="github" src={githubIcon} alt={"github"} /></a>
+                </div>
+              </div>
+              {/* navigation */}
+              <div className="row" id="nav">
+                <nav className="navbar mx-auto">
+                  <NavLink activeClassName='is-active' to='/who'> <button className="btn btn-outline-dark" id="who" type="button">who </button> </NavLink>
+                  <NavLink activeClassName='is-active' to='/what'> <button className="btn btn-outline-dark" id="what" type="button">what </button> </NavLink>
+                  <NavLink activeClassName='is-active' to='/why'> <button className="btn btn-outline-dark" id="why" type="button">why</button> </NavLink>
+                </nav>
+              </div>
+              {/* content */}
+              <div className="row" id="siteBody">
+                <Switch>
+                  <Route exact path='/' render={() => (
+                    <Home />
+                  )} />
+                  <Route exact path='/who' render={() => (
+                    <Who />
+                  )} />
+                  <Route exact path='/what' render={() => (
+                    <What />
+                  )} />
+                  <Route exact path='/why' render={() => (
+                    <Why />
+                  )} />
+                  <Route render={() => (
+                    <h1>Not Found</h1>
+                  )} />
+                </Switch>
+              </div>
+            </div>
+          </div>
+          {/* footer */}
+          <footer>
+            <div className="container" id="footer">
+              <div className="col-md-12">
+                <a id="ftr">created using <strong>React</strong></a>
+              </div>
+            </div>
+          </footer>
+        </div>
+      );
+    }
+
   }
 }
 
--- src/home/Home.js
+++ src/home/Home.js
@@ -2,39 +2,64 @@ import React, { Component } from 'react';
 import './Home.css';
 
 class Home extends Component {
+    constructor() {
+        super();
+        this.state = {
+            width: window.innerWidth,
+        };
+    }
+    componentWillMount() {
+        window.addEventListener('resize', this.handleWindowSizeChange);
+    }
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.handleWindowSizeChange);
+    }
+    handleWindowSizeChange = () => {
+        this.setState({ width: window.innerWidth });
+    };
     render() {
-        return (
-            <div className="mx-auto" id="home">
-                <div className="col-md-12" id="collage">
-                    <a id="n">producer</a>
-                    <a id="m"><strong>EMPATHETIC</strong></a>
-                    <a id="l">visual</a>
+        const { width } = this.state;
+        const isMobile = width < 1000;
+        if (isMobile) {
+            return (
+                <div className="col-md-12">
+                <a id="mobileHome"><mark id="mobileMarkA">Developer</mark> + <mark id="mobileMarkB">Designer</mark></a>
                 </div>
-                <br />
-                <div className="col-md-12" id="collage">
-                    <a id="j"><strong>designer</strong></a>
-                    <a id="k">&nbsp;PROBLEM SOLVER</a>
-                    <a id="i"><strong>human</strong></a>
+            );
+        } else {
+            return (
+                <div className="mx-auto" id="home">
+                    <div className="col-md-12" id="collage">
+                        <a id="n">producer</a>
+                        <a id="m"><strong>EMPATHETIC</strong></a>
+                        <a id="l">visual</a>
+                    </div>
+                    <br />
+                    <div className="col-md-12" id="collage">
+                        <a id="j"><strong>designer</strong></a>
+                        <a id="k">&nbsp;PROBLEM SOLVER</a>
+                        <a id="i"><strong>human</strong></a>
+                    </div>
+                    <br />
+                    <div className="col-md-12" id="collage">
+                        <a id="h"><strong>&nbsp;MODERN</strong></a>
+                        <a id="g"><strong>CODER</strong></a>
+                    </div>
+                    <br />
+                    <div className="col-md-12" id="collage">
+                        <a id="f">storyteller</a>
+                        <a id="e"><strong>MUSICIAN</strong></a>
+                        <a id="d">writer</a>
+                    </div>
+                    <br />
+                    <div className="col-md-12" id="collage">
+                        <a id="c"><strong>creative</strong></a>
+                        <a id="b">inquisitive</a>
+                        <a id="a">ARTIST</a>
+                    </div>
                 </div>
-                <br />
-                <div className="col-md-12" id="collage">
-                    <a id="h"><strong>&nbsp;MODERN</strong></a>
-                    <a id="g"><strong>CODER</strong></a>
-                </div>
-                <br />
-                <div className="col-md-12" id="collage">
-                    <a id="f">storyteller</a>
-                    <a id="e"><strong>MUSICIAN</strong></a>
-                    <a id="d">writer</a>
-                </div>
-                <br />
-                <div className="col-md-12" id="collage">
-                    <a id="c"><strong>creative</strong></a>
-                    <a id="b">inquisitive</a>
-                    <a id="a">ARTIST</a>
-                </div>
-            </div>
-        );
+            );
+        }
     }
 }
 
